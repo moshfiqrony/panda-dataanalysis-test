4331c60d114c0cc4aac7bd26f4adb938236594ba
moshfiqrony <moshfiqrony@gmail.com>
2019-06-27 11:55:17 +0600

    conditonal data framing

--- analysis_001.py
+++ analysis_001.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv('./data.csv')
-# printing the csv file that is read by the reac_csv using pandas
-print(df)
 
 #reading accessing specific field and create a new data_frame and export to csv
+new_df = df.head(3)
+#conditional data framing
+#in this section the data frame will be showing the 3 
+# collumns and based on the given conditions
+
+print(new_df[['_', '_001', '_002/price']][new_df['_002/price'] == True])
