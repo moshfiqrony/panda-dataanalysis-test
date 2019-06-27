c01669f457c488df607e51cd5cf92e8daf976a55
moshfiqrony <moshfiqrony@gmail.com>
2019-06-27 11:43:57 +0600

    frist simple analysis learning

---
+++ analysis_001.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('./data.csv')
+# printing the csv file that is read by the reac_csv using pandas
+print(df)
+
+#reading accessing specific field and create a new data_frame and export to csv
